6eab4f1628f4609bd8b0a9024774a1756647dc63
unchama <unchama@users.noreply.github.com>
2022-06-07 12:04:15 +0900

    git clone先を明示的に指定する

--- scripts/k8s-node-setup.sh
+++ scripts/k8s-node-setup.sh
@@ -382,7 +382,7 @@ EOF
 sudo apt-get install -y ansible git sshpass
 
 # clone repo
-git clone https://github.com/unchama/kube-cluster-on-proxmox.git
+git clone https://github.com/unchama/kube-cluster-on-proxmox.git "$HOME"
 
 # export ansible.cfg target
 export ANSIBLE_CONFIG="$HOME"/kube-cluster-on-proxmox/ansible/ansible.cfg
