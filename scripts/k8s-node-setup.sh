1b31fa06f7560e3765af0600c4eaaf50817b0958
unchama <11990197+unchama@users.noreply.github.com>
2022-09-28 18:02:36 +0900

    fix: namespaceの指定を削除

--- scripts/k8s-node-setup.sh
+++ scripts/k8s-node-setup.sh
@@ -358,7 +358,6 @@ helm install argocd argo/argo-cd \
     --values https://raw.githubusercontent.com/unchama/kube-cluster-on-proxmox/"${TARGET_BRANCH}"/k8s-manifests/argocd-helm-chart-values.yaml
 helm install argocd argo/argocd-apps \
     --version 0.0.1 \
-    --namespace argocd \
     --values https://raw.githubusercontent.com/unchama/kube-cluster-on-proxmox/"${TARGET_BRANCH}"/k8s-manifests/argocd-apps-helm-chart-values.yaml
 
 
